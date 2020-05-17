70ab7da738e836187d2fc5ff3fae1413065166af
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-05-17 21:22:37 +0700

    Update MamenMager.py

--- MamenMager.py
+++ MamenMager.py
@@ -17,7 +17,7 @@ else:
         os.system('pip2 install requests')
         
 def entertools():
-	os.system('sh babi_buat_yang_ngerecode_thanks.sh')
+	os.sys 
 
 def restart():
     ngulang = sys.executable
